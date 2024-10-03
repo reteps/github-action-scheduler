0ca77e43f86bfa08c4a2cbe68273644fd33cd58a
Pete Stenger <pete@stenger.io>
2024-10-03 18:38:59 -0500

    init

--- src/__tests__/add.test.ts
+++ src/__tests__/add.test.ts
@@ -1,8 +1,68 @@
 import { describe, it, expect } from 'vitest';
-import { add } from '../index';
+import { scheduleJobs, Job } from '../index';
 
+const exampleJob: Job = {
+  name: 'Deploy (fallctf.com)',
+  'runs-on': 'ubuntu-latest',
+  permissions: {
+    contents: 'read',
+    deployments: 'write',
+  },
+  steps: [
+    {
+      name: 'Check out repository code',
+      uses: 'actions/checkout@v3',
+      with: {
+        submodules: true,
+        'fetch-depth': 0,
+      },
+    },
+    {
+      name: 'Build 2023 mdbook',
+      uses: './.github/actions/build-mdbook',
+      with: {
+        'book-directory': 'fallctf-2023',
+      },
+    },
+    {
+      name: 'Build 2024 mdbook',
+      uses: './.github/actions/build-mdbook',
+      with: {
+        'book-directory': 'fallctf-2024',
+      },
+    },
+    {
+      name: 'Build site',
+      uses: './.github/actions/build-astro',
+      with: {
+        'working-directory': '${{ env.CI_WORKING_DIR }}',
+        'build-directory': '${{ env.CI_BUILD_DIR }}',
+        'cache-directory': '${{ env.CI_CACHE_DIR }}',
+      },
+    },
+    {
+      name: 'Deploy to Cloudflare Pages',
+      if: "github.actor != 'dependabot[bot]'",
+      uses: 'cloudflare/pages-action@v1',
+      with: {
+        apiToken: '${{ secrets.CLOUDFLARE_PAGES_API_TOKEN }}',
+        accountId: '${{ secrets.CLOUDFLARE_ACCOUNT_ID }}',
+        projectName: '${{ vars.CLOUDFLARE_PROJECT_NAME_FALLCTF }}',
+        gitHubToken: '${{ secrets.GITHUB_TOKEN }}',
+        workingDirectory: '${{ env.CI_WORKING_DIR }}',
+        directory: '${{ env.CI_BUILD_DIR }}',
+      },
+    },
+  ],
+};
 describe('add', () => {
-  it('should add two numbers', () => {
-    expect(add(1, 1)).toBe(2);
+  it('should add two numbers', async () => {
+    scheduleJobs([
+      { time: new Date('2021-01-01T00:00:00Z'), ...exampleJob },
+    ], {
+      'path': 'workflow.yml',
+      'check': false,
+      'merge': false,
+    })
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,162 @@
-export function add(x: number, y: number): number {
-  return x + y;
+import { load, dump } from "js-yaml";
+import { readFile, writeFile, mkdir } from "fs/promises";
+import { GeneratedWorkflowTypes } from 'github-actions-workflow-ts'
+import { resolve, dirname } from "path";
+import { existsSync } from "fs";
+import slugify from "slugify";
+type Workflow = GeneratedWorkflowTypes.Workflow;
+export type Job = GeneratedWorkflowTypes.ReusableWorkflowCallJob | GeneratedWorkflowTypes.NormalJob;
+
+export const checkPath = (path: string): void => {
+  if (!path.endsWith(".yml")) {
+    throw new Error("File must be a YAML file");
+  }
+  if (!path.includes(".github/workflows/")) {
+    throw new Error("File must be in the .github/workflows directory");
+  }
+  const absolutePath = resolve(path);
+  const gitPath = absolutePath.replace(/\.github.*/, ".git/");
+  
+  if (!existsSync(gitPath)) {
+    throw new Error(`Path ${absolutePath} does not appear to be in a git repository`);
+  }
+}
+
+export async function loadYaml(path: string, check: boolean = true): Promise<Workflow | undefined> {
+  if (check) {
+    checkPath(path);
+  }
+
+  if (!existsSync(path)) {
+    return undefined;
+  }
+
+  const content = await readFile(path, "utf8");
+  return load(content) as Workflow;
+}
+
+export async function saveYaml(path: string, content: Workflow, check: boolean = true): Promise<void> {
+  if (check) {
+    checkPath(path);
+  }
+
+  const preferredKeys = ['name', 'on', 'if']
+  const yaml = dump(content, {
+    lineWidth: -1,
+    sortKeys: (a, b) => {
+      let aIdx = preferredKeys.indexOf(a);
+      let bIdx = preferredKeys.indexOf(b);
+      aIdx = aIdx === -1 ? preferredKeys.length : aIdx;
+      bIdx = bIdx === -1 ? preferredKeys.length : bIdx;
+      if (aIdx !== bIdx) {
+        return aIdx - bIdx;
+      }
+
+      return a.localeCompare(b);
+    }
+  });
+  await mkdir(dirname(path), { recursive: true }) 
+  await writeFile(path, yaml);
+}
+
+const dateToCron = (date: Date) => {
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const days = date.getDate();
+  const months = date.getMonth() + 1;
+  const dayOfWeek = date.getDay();
+
+  return `${minutes} ${hours} ${days} ${months} ${dayOfWeek}`;
+};
+
+type TimedJob = Job & {
+  time: Date;
+}
+
+const isEvent = (on: any): on is GeneratedWorkflowTypes.Event => {
+  return typeof on === "string";
+}
+const isEventList = (on: any): on is GeneratedWorkflowTypes.Event[] => {
+  return Array.isArray(on) && on.every(isEvent);
+}
+const isEventObject = (on: any): on is Exclude<Workflow["on"], GeneratedWorkflowTypes.Event | GeneratedWorkflowTypes.Event[]> => {
+  return typeof on === "object";
+}
+
+interface JobScheduleOptions {
+  path: string; // Path to the workflow file
+  merge?: boolean; // Should we override the existing workflow, or merge in the new jobs?
+  check?: boolean; // Should we check if the path is a valid path?
+}
+
+export const scheduleJobs = async (jobs: [TimedJob, ...TimedJob[]], options: JobScheduleOptions) => {
+  const { path, check = true, merge = false } = options;
+  const workflow = (await loadYaml(path, check)) || {} as Workflow;
+
+  // Set defaults so that we can assume they exist.
+  workflow.name = workflow.name || "Scheduled Jobs";
+  workflow.on = workflow.on || {};
+
+  // workflow.on is either single event or array of events.
+  // if it's a single event, we need to convert it to an array.
+  // exclude Event or Event[] from the type.
+  const schedules = jobs.map((job) => ({
+      cron: dateToCron(job.time),
+  })) as [
+    {
+        cron?: string;
+    },
+    ...{
+        cron?: string;
+    }[]
+  ];
+
+  if (!merge) {
+    workflow.on = { schedule: schedules };
+  } else {
+    if (isEvent(workflow.on)) {
+      workflow.on = { [workflow.on]: null, schedule: schedules };
+    } else if (isEventList(workflow.on)) {
+      workflow.on = {
+        ...workflow.on.reduce((acc, event) => ({ ...acc, [event]: null }), {}),
+        schedule: schedules,
+      };
+    } else if (isEventObject(workflow.on)) {
+      const uniqueCrons = [
+        ...schedules,
+        ...(workflow.on.schedule || []),
+      ].filter((schedule, index, self) => self.findIndex((s) => s.cron === schedule.cron) === index) as [
+        {
+            cron?: string;
+        },
+        ...{
+            cron?: string;
+        }[]
+      ];
+  
+      workflow.on = {
+        ...workflow.on, 
+        schedule: uniqueCrons,
+      }
+    }
+  }
+
+  // Override if guard
+  const namedConditionedJobs = jobs.map((job) => {
+    const { time, if: _if, name: _name, ...rest } = job;
+    return {
+      name: job.name || `Scheduled job at ${job.time.toISOString()}`,
+      if: `github.event.schedule == '${dateToCron(time)}'`,
+      ...rest,
+    };
+  }).reduce((acc, job) => {
+    acc[slugify(job.name || 'unknown')] = job;
+    return acc;
+  }, {} as Record<string, Job>);
+
+  workflow.jobs = {
+    ...namedConditionedJobs,
+    ...((merge) ? workflow.jobs : {}),
+  };
+  await saveYaml(path, workflow, check);
 }
