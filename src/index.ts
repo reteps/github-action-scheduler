f07fddcccf16eb719112257c1fe6f177c4bdf339
Pete Stenger <pete@stenger.io>
2024-10-03 22:49:00 -0500

    add space

--- src/index.ts
+++ src/index.ts
@@ -145,7 +145,7 @@ export const scheduleJobs = async (jobs: [TimedJob, ...TimedJob[]], options: Job
   const namedConditionedJobs = jobs.map((job) => {
     const { time, if: _if, name: _name, ...rest } = job;
     return {
-      name: (job.name || 'Scheduled job') + `at ${job.time.toISOString()}`,
+      name: (job.name || 'Scheduled job') + ` at ${job.time.toISOString()}`,
       if: `github.event.schedule == '${dateToCron(time)}'`,
       ...rest,
     };
